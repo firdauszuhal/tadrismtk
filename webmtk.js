f02f97398cd0159e567a83454b3fb50869373a70
firdauszuhal <42400377+firdauszuhal@users.noreply.github.com>
2018-08-15 14:05:08 +0700

    Create webmtk.js

---
+++ webmtk.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+$('body').css("background-image", "url('http://parade.com/wp-content/uploads/2013/05/maths-ftr.jpg')");
+$("#lowongan").hide();
+$("#a_middle").hide();
+$("#event").hide();
+$("#pmb").hide();
+}
