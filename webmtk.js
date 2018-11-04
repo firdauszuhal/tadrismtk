d01a3c21ee2ff1f8bb921f099244ce83d7e118d5
firdauszuhal <42400377+firdauszuhal@users.noreply.github.com>
2018-11-04 13:41:39 +0700

    Update webmtk.js

--- webmtk.js
+++ webmtk.js
@@ -1,5 +1,4 @@
 window.onload = function () {
-$('body').css("background-image", "url('http://parade.com/wp-content/uploads/2013/05/maths-ftr.jpg')");
 $("#lowongan").hide();
 $("#a_middle").hide();
 $("#event").hide();
